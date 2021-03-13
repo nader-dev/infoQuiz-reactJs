bac5d34d1faf5b1b6a6abcdf1f986a65c8009b8c
nader <hammami.ndr@gmail.com>
2021-03-13 01:19:32 +0100

    add components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <div className="footer-container">
+                    <p>Projet rélalisé par hammami nader 2021</p>
+                    <p>les icone mettre ici</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
