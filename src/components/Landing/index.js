bcf5b32dda420f4cfd0c68b808f7402e6a4c1899
nader <hammami.ndr@gmail.com>
2021-03-13 01:53:55 +0100

    add state and test button show

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
-const Landing    = () => {
+const Landing = () => {
+    const refInfiQuiz = useRef(null)
+
+    const [btn, setBtn] = useState(false)
+
+    useEffect(() => {
+        refInfiQuiz.current.classList.add("startingImg")
+        setTimeout(() => {
+            refInfiQuiz.current.classList.remove("startingImg")
+            setBtn(true)
+        }, 1000);
+    }, [])
+
+
     return (
         <div>
-            <main className="welcomePage">
-
+            <main ref={refInfiQuiz} className="welcomePage">
+                {btn ?
+                    <>
+                        <div className="leftBox">
+                            <button className="btn-welcome">Inscription</button>
+                        </div>
+                        <div className="rightBox">
+                            <button className="btn-welcome">Connexion</button>
+                        </div>
+                    </>
+                    : null}
             </main>
         </div>
     )
